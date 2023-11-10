575b383575c13cbd90b33c1f910e0e2eaec52c24
MateoMajlin <majlinmateo@gmail.com>
2023-11-10 11:52:29 -0300

    Juegos K tomando forma

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import "./App.css";
 function App() {
    return (
-    <h1>Juegos K</h1>
+      <>
+      <div className="body">
+      <div className="superior">
+      <h2 className="top">Inicio</h2><h2 className="top">Cuenta</h2><h2 className="top">Easter</h2>
+      </div>
+      <div>
+         <h1>Juegos K</h1>
+      </div>
+      <h2 className="etiqueta">1 Jugador</h2>
+      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      <br></br>
+      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      <h2 className="etiqueta">2 Jugadores</h2>
+      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      <br></br>
+      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      <div className="juego"><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"></img></div>
+      </div>
+      </>
    )
 }
 export default App;
