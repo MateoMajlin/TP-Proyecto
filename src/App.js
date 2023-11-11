7ace468cb7998a85b9494f960dee272bb65fdf47
MateoMajlin <majlinmateo@gmail.com>
2023-11-10 22:02:22 -0300

    Ahi tenes luquitas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h2 className="top"><Link to="/">Inicio</Link></h2><h2 className="top"><Link to="/Cuenta">Cuenta</Link></h2><h2 className="top"><Link to="/Easter">Easter</Link></h2>
       </div>
       <div>
-         <h1><marquee>Juegos K</marquee></h1>
+         <h1>Juegos K</h1>
       </div>
       <h2 className="etiqueta">1 Jugador</h2>
       <div className="juego"><Link to="https://www.txori.com/dbdevolution"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNF-1cfNRlyGgOT_4emHVmZpewEGjSXtXzmIrkE_Uldyawa6VwiC7jGfD7pxtprHCJ0vc&usqp=CAU" alt="pikapika"></img></Link></div>
