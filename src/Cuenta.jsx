8c29730678b8ed097be2557e54bf481a0a61e778
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-11 19:49:05 -0300

    Update Cuenta.jsx

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -8,4 +8,26 @@ function Cuenta() {
         </>
     )
 }
+
+function LoginPage () {
+
+return (
+
+<div className = «loginPage»>
+
+<h1 className = «loginPage-title»>Log in to Twitter</h1>
+
+<form>
+
+<input type = «text» name= «username»></input>
+
+<input type= «password» name= «password»/>
+
+</form>
+
+</div>
+
+);
+
+}
 export default Cuenta;
