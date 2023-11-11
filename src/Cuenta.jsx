2e04dabc4b3bfaf8cc4916d04d8a20a291e3292b
MateoMajlin <majlinmateo@gmail.com>
2023-11-10 21:58:06 -0300

    Ahi tenes luquitas

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import cuenta from "./Cuenta.module.css";
 function Cuenta() {
     return(
         <>
         <h2 className="top"><Link to="/">Inicio</Link></h2>
-        <h1>Cuenta lul</h1>
+        <h1 className={cuenta.merengue}>Cuenta lul</h1>
         </>
     )
 }
--- src/Easter.jsx
+++ src/Easter.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "./Easter.css";
-function Easter(){
-    return(
-        <div className='fondoeaster'>
-        <h2 id='egg'><Link to="/">Inicio</Link></h2>
-        </div>
-    )
+import easter from "./Easter.module.css";
+
+function Easter() {
+  return (
+    <div className={easter.fondoeaster}>
+      <h2 id={easter.egg}><Link to="/">Inicio</Link></h2>
+    </div>
+  );
 }
+
 export default Easter;
+
