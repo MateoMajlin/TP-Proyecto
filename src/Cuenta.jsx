4e02e197a794cc57f3160c2118b6b5962bd63447
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-13 21:32:24 -0300

    Update Cuenta.jsx

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
 import cuenta from "./Cuenta.module.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 function Cuenta() {
     return(
         <>
@@ -8,4 +11,29 @@ function Cuenta() {
         </>
     )
 }
+function BasicExample() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
 export default Cuenta;
