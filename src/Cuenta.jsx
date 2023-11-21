6bce6ec0bf279094110e56b727c583f97d7a6294
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-21 14:22:36 -0300

    Update Cuenta.jsx

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -7,7 +7,6 @@ import Form from 'react-bootstrap/Form';
 function Cuenta() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loggedIn, setLoggedIn] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
   const handleEmailChange = (e) => {
@@ -21,15 +20,16 @@ function Cuenta() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Simulamos una autenticación exitosa si ambos campos no están vacíos
     if (email.trim() !== '' && password.trim() !== '') {
-      setLoggedIn(true);
+      // Simula la autenticación exitosa y guarda el usuario en localStorage
+      const user = { email, password };
+      localStorage.setItem('user', JSON.stringify(user));
+
       setShowSuccessMessage(true);
 
-      // Resetear showSuccessMessage después de un tiempo para que no se muestre permanentemente
       setTimeout(() => {
         setShowSuccessMessage(false);
-      }, 3000); // Mostrar el mensaje durante 3 segundos, ajusta según sea necesario
+      }, 3000);
     } else {
       alert('Por favor, ingresa tu correo electrónico y contraseña.');
     }
@@ -37,52 +37,10 @@ function Cuenta() {
 
   return (
     <>
-      <div className={cuentaStyles["cuenta-container"]}>
-        <h2 className={cuentaStyles["cuenta-top"]}><Link to="/">Inicio</Link></h2>
-        <h1 className={cuentaStyles["cuenta-merengue"]}>Cuenta lul</h1>
-        {loggedIn ? (
-          <p className={cuentaStyles["cuenta-success-message"]}>
-            
-          </p>
-        ) : (
-          <Form className={cuentaStyles["cuenta-form"]} onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label className={cuentaStyles["cuenta-label"]}>Email address</Form.Label>
-              <Form.Control
-                className={cuentaStyles["cuenta-control"]}
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={handleEmailChange}
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label className={cuentaStyles["cuenta-label"]}>Password</Form.Label>
-              <Form.Control
-                className={cuentaStyles["cuenta-control"]}
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button className={cuentaStyles["cuenta-button"]} variant="primary" type="submit">
-              Iniciar Sesión
-            </Button>
-          </Form>
-        )}
-
+      {/* Tu código actual aquí */}
+      <div>
         {showSuccessMessage && (
-          <p className={cuentaStyles["cuenta-success-message"]}>
-            ¡Bienvenido, has iniciado sesión exitosamente!
-          </p>
+          <p>¡Bienvenido, has iniciado sesión exitosamente!</p>
         )}
       </div>
     </>
@@ -90,3 +48,4 @@ function Cuenta() {
 }
 
 export default Cuenta;
+
