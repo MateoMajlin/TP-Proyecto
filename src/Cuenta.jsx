ce969ef3c114ea9dedbb5f43748ee698fd2e65bb
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-21 14:28:53 -0300

    Update Cuenta.jsx

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -37,7 +37,47 @@ function Cuenta() {
 
   return (
     <>
-      {/* Tu código actual aquí */}
+      <div className={cuentaStyles["cuenta-container"]}>
+        <h2 className={cuentaStyles["cuenta-top"]}><Link to="/">Inicio</Link></h2>
+        <h1 className={cuentaStyles["cuenta-merengue"]}>Cuenta lul</h1>
+        {loggedIn ? (
+          <p className={cuentaStyles["cuenta-success-message"]}>
+            
+          </p>
+        ) : (
+          <Form className={cuentaStyles["cuenta-form"]} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className={cuentaStyles["cuenta-label"]}>Email address</Form.Label>
+              <Form.Control
+                className={cuentaStyles["cuenta-control"]}
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label className={cuentaStyles["cuenta-label"]}>Password</Form.Label>
+              <Form.Control
+                className={cuentaStyles["cuenta-control"]}
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button className={cuentaStyles["cuenta-button"]} variant="primary" type="submit">
+              Iniciar Sesión
+            </Button>
+          </Form>
+        )}
       <div>
         {showSuccessMessage && (
           <p>¡Bienvenido, has iniciado sesión exitosamente!</p>
