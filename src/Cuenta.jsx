7143d7e74612a2f794f31316da1637a0773d5f37
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-21 14:32:17 -0300

    Update Cuenta.jsx mdfsg

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -1,8 +1,5 @@
-import { Link } from 'react-router-dom';
-import cuentaStyles from "./Cuenta.module.css";
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+// Cuenta.jsx
+import React, { useState } from 'react';
 
 function Cuenta() {
   const [email, setEmail] = useState('');
@@ -36,7 +33,58 @@ function Cuenta() {
   };
 
   return (
-    <>
+<>
+ <div className={cuentaStyles["cuenta-container"]}>
+        <h2 className={cuentaStyles["cuenta-top"]}><Link to="/">Inicio</Link></h2>
+        <h1 className={cuentaStyles["cuenta-merengue"]}>Cuenta lul</h1>
+        {loggedIn ? (
+          <p className={cuentaStyles["cuenta-success-message"]}>
+            
+          </p>
+        ) : (
+          <Form className={cuentaStyles["cuenta-form"]} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className={cuentaStyles["cuenta-label"]}>Email address</Form.Label>
+              <Form.Control
+                className={cuentaStyles["cuenta-control"]}
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label className={cuentaStyles["cuenta-label"]}>Password</Form.Label>
+              <Form.Control
+                className={cuentaStyles["cuenta-control"]}
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button className={cuentaStyles["cuenta-button"]} variant="primary" type="submit">
+              Iniciar Sesión
+            </Button>
+          </Form>
+        )}
+      <div>
+        {showSuccessMessage && (
+          <p>¡Bienvenido, has iniciado sesión exitosamente!</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Cuenta;
       <div className={cuentaStyles["cuenta-container"]}>
         <h2 className={cuentaStyles["cuenta-top"]}><Link to="/">Inicio</Link></h2>
         <h1 className={cuentaStyles["cuenta-merengue"]}>Cuenta lul</h1>
