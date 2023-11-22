e1a439bc86310f8fb7a680ba7ec4c7a5d927ff02
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-22 16:24:13 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
  import React from 'react';
 import { Link } from 'react-router-dom';
-import estilos from "./App.module.css"; // Asegúrate de importar el archivo CSS correctamente
+import estilos from "./App.module.css"; 
 import JuegoMemoria from "./JuegoMemoria";
 
 function MensajeBienvenida({ usuario }) {
