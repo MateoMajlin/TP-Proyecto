2a1d66d9dea0b212bc98c7dabc783d557c14cb71
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-22 00:17:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import estilos from "./App.module.css"; // Asegúrate de importar el archivo CSS
 
 function MensajeBienvenida({ usuario }) {
   if (usuario) {
-    return <p>Bienvenido, {usuario.email}.</p>;
+    return <p className={estilos.bienvenida}>Bienvenido, {usuario.email}.</p>;
+  }
+  return null;
+}
+
+function NombreUsuarioDestacado({ usuario }) {
+  if (usuario) {
+    return <h3 className={estilos.nombreUsuarioDestacado}>USUARIO={usuario.email}</h3>;
   }
   return null;
 }
@@ -22,6 +29,7 @@ function App() {
         </div>
 
         <MensajeBienvenida usuario={usuario} />
+        <NombreUsuarioDestacado usuario={usuario} />
 
         <h1>Juegos K</h1>
 
