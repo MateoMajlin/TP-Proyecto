175685e429bee9fd5b89b5578d4d4596c57c44e1
MateoMajlin <majlinmateo@gmail.com>
2023-11-23 21:56:11 -0300

    Ahi tenes luquitas

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 import cuenta from "./Cuenta.module.css";
-function Cuenta() {
-    return(
-        <>
-        <h2 className="top"><Link to="/">Inicio</Link></h2>
-        <h1 className={cuenta.merengue}>Cuenta lul</h1>
-        </>
-    )
+function Cuenta(){
+ return (
+    <>
+    <h1 className={cuenta.left}>Inicio</h1><h1 className={cuenta.center}>Bienvenido a Cuenta</h1>
+    <div>
+        <img src='https://media.tenor.com/yTHoLS82szwAAAAM/pato-girando.gif' alt='patogirando'></img>
+    </div>
+    <Link to="./IniciarSesion"><h1>Iniciar Sesion</h1></Link>
+    <Link to="./CrearCuenta"><h1>Crear Cuenta</h1></Link>
+    </>
+ );
 }
 export default Cuenta;
--- src/rutas.jsx
+++ src/rutas.jsx
@@ -2,17 +2,19 @@ import {Routes, Route } from "react-router-dom";
 import App from "./App";
 import Cuenta from "./Cuenta";
 import Easter from "./Easter";
-
+import CrearCuenta from "./CrearCuenta";
+import IniciarSesion from "./IniciarSesion";
+import { useState } from "react";
 
 function Rutas() {
-
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
     <>
     <Routes>
     <Route
         exact
         path="/"
-        element={<App />}
+        element={<App isLoggedIn={isLoggedIn}/>}
       />
       <Route
         exact
@@ -23,6 +25,16 @@ function Rutas() {
         exact
         path="/Easter"
         element={<Easter />}
+      />
+      <Route
+        exact
+        path="/Cuenta/IniciarSesion"
+        element={<IniciarSesion isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn}/>}
+      />
+      <Route
+        exact
+        path="/Cuenta/CrearCuenta"
+        element={<CrearCuenta />}
       />
     </Routes>
     </>
