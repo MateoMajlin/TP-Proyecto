ca167be2aedd2ebd218f2430b6f085deb8c46752
MateoMajlin <majlinmateo@gmail.com>
2023-11-23 22:20:33 -0300

    a ver

--- src/rutas.jsx
+++ src/rutas.jsx
@@ -2,23 +2,15 @@ import {Routes, Route } from "react-router-dom";
 import App from "./App";
 import Cuenta from "./Cuenta";
 import Easter from "./Easter";
-<<<<<<< HEAD
-import CrearCuenta from "./CrearCuenta";
-import IniciarSesion from "./IniciarSesion";
-import { useState } from "react";
-=======
-import JuegoMemoria from "./JuegoMemoria"
->>>>>>> 9597b4a4db3549b236bb5aa697bb80bdf2d78dcc
 
 function Rutas() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
     <>
     <Routes>
     <Route
         exact
         path="/"
-        element={<App isLoggedIn={isLoggedIn}/>}
+        element={<App />}
       />
       <Route
         exact
@@ -30,22 +22,6 @@ function Rutas() {
         path="/Easter"
         element={<Easter />}
       />
-      <Route
-<<<<<<< HEAD
-        exact
-        path="/Cuenta/IniciarSesion"
-        element={<IniciarSesion isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn}/>}
-      />
-      <Route
-        exact
-        path="/Cuenta/CrearCuenta"
-        element={<CrearCuenta />}
-      />
-=======
-        exact path= "/JuegoDosJugadores"
-        element={<JuegoDosJugadores />}
-          />
->>>>>>> 9597b4a4db3549b236bb5aa697bb80bdf2d78dcc
     </Routes>
     </>
   );
