4de7c2d0c85263b3974ac7b1f216c7e3b61efb4f
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-25 01:02:16 -0300

    Create CalculadoraPorcentajes.jsx

---
+++ src/juego/CalculadoraPorcentajes.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import styles from "./JuegoRaro.module.css";
+
+const CalculadoraPorcentajes = () => {
+  const [cantidad, setCantidad] = useState("");
+  const [porcentaje, setPorcentaje] = useState("");
+  const [resultado, setResultado] = useState(null);
+
+  const calcularPorcentaje = () => {
+    const cantidadFloat = parseFloat(cantidad);
+    const porcentajeFloat = parseFloat(porcentaje);
+
+    if (!isNaN(cantidadFloat) && !isNaN(porcentajeFloat)) {
+      const resultadoCalculado = (cantidadFloat * porcentajeFloat) / 100;
+      setResultado(resultadoCalculado);
+    } else {
+      setResultado(null);
+    }
+  };
+
+  return (
+    <div className={styles["calculadora-container"]}>
+      <h1>Calculadora de Porcentajes del Pibe K</h1>
+      <div className={styles["input-container"]}>
+        <label>Cantidad:</label>
+        <input
+          type="number"
+          value={cantidad}
+          onChange={(e) => setCantidad(e.target.value)}
+        />
+      </div>
+      <div className={styles["input-container"]}>
+        <label>Porcentaje:</label>
+        <input
+          type="number"
+          value={porcentaje}
+          onChange={(e) => setPorcentaje(e.target.value)}
+        />
+      </div>
+      <button onClick={calcularPorcentaje}>Calcular</button>
+      {resultado !== null && (
+        <div className={styles["resultado"]}>
+          <p>
+            El {porcentaje}% de {cantidad} es:
+          </p>
+          <p>{resultado}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CalculadoraPorcentajes;
