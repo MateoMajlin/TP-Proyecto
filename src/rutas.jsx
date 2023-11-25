76d1adb4433736551b313a0614505d0053d63b66
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-24 21:35:31 -0300

    Update rutas.jsx

--- src/rutas.jsx
+++ src/rutas.jsx
@@ -1,49 +1,93 @@
 import {Routes, Route } from "react-router-dom";
-import App from "./App";
+
+import App from "./App"; 
+
 import Cuenta from "./Cuenta";
+
 import Easter from "./Easter";
+
 import IniciarSesion from "./IniciarSesion";
+
 import CrearCuenta from "./CrearCuenta";
+
 import Adivinanza from "./Adivinanza";
 
 function Rutas() {
+
   return (
+
     <>
-    <Routes>
-    <Route
-        exact
-        path="/"
-        element={<App />}
-      />
-      <Route
-        exact
-        path="/Cuenta"
-        element={<Cuenta />}
-      />
-      <Route
-        exact
-        path="/Easter"
-        element={<Easter />}
-      />
-      <Route
-        exact
-        path="/Cuenta/IniciarSesion"
-        element={<IniciarSesion />}
-      />
-      <Route
-        exact
-        path="/Cuenta/CrearCuenta"
-        element={<CrearCuenta />}
-      />
-    </Routes>
-       <Route
-        exact
-        path="/Adivinanza"
-        element={<Adivinanza />}
-      />
-    </Routes>
+
+      <Routes>
+
+        <Route  
+
+          exact 
+
+          path="/"  
+
+          element={<App />} 
+
+        />  
+
+        <Route
+
+          exact
+
+          path="/Cuenta"
+
+          element={<Cuenta />}
+
+        />
+
+        <Route  
+
+          exact
+
+          path="/Easter"  
+
+          element={<Easter />}  
+
+        />
+
+        <Route  
+
+          exact
+
+          path="/Cuenta/IniciarSesion"
+
+          element={<IniciarSesion />}
+
+        />
+
+        <Route  
+
+          exact
+
+          path="/Cuenta/CrearCuenta"  
+
+          element={<CrearCuenta />}  
+
+        />
+
+       <Route  
+
+          exact
+
+          path="/Adivinanza"  
+
+          element={<Adivinanza />}
+
+        />
+
+      </Routes>
+
     </>
+
   );
+
 }
+
+
 
 export default Rutas;
