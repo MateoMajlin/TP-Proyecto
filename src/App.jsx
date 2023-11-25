3ba6e816effb6e63e83b78edfe40e99468bc8299
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-24 22:14:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,108 @@
-import  "./App.css";
-import { Link } from 'react-router-dom';
-import "./Adivinanza.jsx";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+
 function App() {
-   return (
-      <>
+  return (
+    <>
       <div className="body">
-      <div className="superior">
-      <h2 className="top"><Link to="/">Inicio</Link></h2><h2 className="top"><Link to="/Cuenta">Cuenta</Link></h2><h2 className="top"><Link to="/Easter">Easter</Link></h2>
+        <div className="superior">
+          <h2 className="top">
+            <Link to="/">Inicio</Link>
+          </h2>
+          <h2 className="top">
+            <Link to="/Cuenta">Cuenta</Link>
+          </h2>
+          <h2 className="top">
+            <Link to="/Easter">Easter</Link>
+          </h2>
+        </div>
+        <div>
+          <h1>Juegos K</h1>
+        </div>
+        <h2 className="etiqueta">1 Jugador</h2>
+        <div className="juego">
+          <Link to="https://www.txori.com/dbdevolution">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNF-1cfNRlyGgOT_4emHVmZpewEGjSXtXzmIrkE_Uldyawa6VwiC7jGfD7pxtprHCJ0vc&usqp=CAU"
+              alt="pikapika"
+            />
+          </Link>
+        </div>
+        <div className="juego">
+          <Link to="./Adivinanza">
+            <img
+              src="https://cdn.pixabay.com/photo/2016/03/31/19/58/riddle-1295268_960_720.png"
+              alt="pikapika"
+            />
+          </Link>
+        </div>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <br></br>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <h2 className="etiqueta">2 Jugadores</h2>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <br></br>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
+        <div className="juego">
+          <img
+            src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
+            alt="pikapika"
+          />
+        </div>
       </div>
-      <div>
-         <h1>Juegos K</h1>
-      </div>
-      <h2 className="etiqueta">1 Jugador</h2>
-      <div className="juego"><Link to="https://www.txori.com/dbdevolution"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNF-1cfNRlyGgOT_4emHVmZpewEGjSXtXzmIrkE_Uldyawa6VwiC7jGfD7pxtprHCJ0vc&usqp=CAU" alt="pikapika"></img></Link></div>
-      <div className="juego"><Link to="./Adivinanza"><img src="https://cdn.pixabay.com/photo/2016/03/31/19/58/riddle-1295268_960_720.png" alt="pikapika"></img></Link></div>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <br></br>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <h2 className="etiqueta">2 Jugadores</h2>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <br></br>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      <div className="juego"><img src="https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="pikapika"></img></div>
-      </div>
-      </>
-   )
+    </>
+  );
 }
+
 export default App;
