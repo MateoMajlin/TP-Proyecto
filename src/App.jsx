afdd7a8183456d60fd8b69ab0dce50df2d5b64e1
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-26 19:19:32 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
             <Link to="/Cuenta">CUENTA</Link>
           </h2>
           <h2 className="top">
-            <Link to="/Easter">EASTER</Link>
+            <Link to="https://www.nyan.cat/">EASTER</Link>
           </h2>
         </div>
       
