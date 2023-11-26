7f71df8ff5a8316624c4749e38893b558d800143
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-26 17:03:52 -0300

    Update Cuenta.jsx

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -1,15 +1,56 @@
-import { Link } from 'react-router-dom';
-import cuenta from "./Cuenta.module.css";
-function Cuenta(){
- return (
-    <>
-    <h1 className={cuenta.left}>Inicio</h1><h1 className={cuenta.center}>Bienvenido a Cuenta</h1>
-    <div>
-        <img src='https://media.tenor.com/yTHoLS82szwAAAAM/pato-girando.gif' alt='patogirando'></img>
-    </div>
-    <Link to="./IniciarSesion"><h1>Iniciar Sesion</h1></Link>
-    <Link to="./CrearCuenta"><h1>Crear Cuenta</h1></Link>
-    </>
- );
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import './Cuenta.module.css'
+
+function Cuenta(props) {
+    const navigate = useNavigate();
+    const {setAdmin} = props;
+    const [pass, setPass] = useState("");
+    const [error,setError] = useState("");
+    const [ver,setVer] = useState("password");
+
+
+    function HandleSubmit(e) {
+        e.preventDefault();
+        if(pass === "react"){
+            setAdmin(true);
+            navigate('/');
+        }else {
+            setAdmin(false)
+        setError(true)
+        }
+        
+    }
+    function ViewPass(e){
+        e.preventDefault()
+    if(ver === "password"){
+    setVer("text")
+ }else{ setVer("password")}
+
+}
+
+    return(
+        <>
+         <header>
+        <img src="https://cdn4.iconfinder.com/data/icons/communication-v2/64/number_numero_count_thirty_five-2-512.png" alt="logo" />
+      <nav className= "nav-menu">
+        <ul>
+          <li>
+            <Link  to="/">Volver a la pagina principal</Link>
+          </li>
+        </ul>
+      </nav>
+      </header>
+      <div className="ContenedorGeneral">
+      {error && (<h1 className= "admin-error">Contraseña incorrecta</h1>)}
+            <form className= "admin-form">
+                <input className= "admin-input" value={pass} type= {ver} placeholder= "Ingresa la contraseña" onChange={e => setPass(e.target.value)} />
+                <button className="admin-button" onClick={ViewPass} >Ver contraseña 🔐</button>
+                <button  className="admin-button"  onClick={HandleSubmit}>Enviar</button>
+            </form></div>
+            
+        </>
+    )
 }
 export default Cuenta;
