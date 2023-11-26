cdcb0d007871a7210342b81b7ebf843e71d47e58
MateoMajlin <130392485+MateoMajlin@users.noreply.github.com>
2023-11-26 17:17:04 -0300

    Update Cuenta.jsx

--- src/Cuenta.jsx
+++ src/Cuenta.jsx
@@ -1,63 +1,86 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import styles from './Cuenta.module.css'; // Asegúrate de tener esta importación
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import './blog.css';
 
-function Cuenta(props) {
+function CrearCuenta() {
   const navigate = useNavigate();
-  const { setAdmin } = props;
-  const [pass, setPass] = useState("");
-  const [error, setError] = useState("");
-  const [ver, setVer] = useState("password");
+  const [nombreUsuario, setNombreUsuario] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [contrasena, setContrasena] = useState("");
+  const [cuentas, setCuentas] = useState([]);
+  const [contador, setContador] = useState(0);
 
-  function HandleSubmit(e) {
+  useEffect(() => {
+    let cuentasGuardadas = JSON.parse(localStorage.getItem('cuentas'));
+    if (cuentasGuardadas) {
+      setCuentas(cuentasGuardadas);
+      const maxId = Math.max(...cuentasGuardadas.map(cuenta => cuenta.id), 0);
+      setContador(maxId + 1);
+    }
+  }, []);
+
+  function handleSubmit(e) {
     e.preventDefault();
-    if (pass === "react") {
-      setAdmin(true);
+    if (nombreUsuario && correo && contrasena) {
+      const id = contador;
+      const nuevaCuenta = { nombreUsuario, correo, contrasena, id };
+      const cuentasActualizadas = [...cuentas, nuevaCuenta];
+      setCuentas(cuentasActualizadas);
+      localStorage.setItem('cuentas', JSON.stringify(cuentasActualizadas));
+      setContador(contador + 1);
+      setNombreUsuario("");
+      setCorreo("");
+      setContrasena("");
+      localStorage.setItem('nombreUsuario', nombreUsuario);
       navigate('/');
-    } else {
-      setAdmin(false);
-      setError("Contraseña incorrecta");
     }
   }
 
-  function ViewPass(e) {
-    e.preventDefault();
-    setVer((prevVer) => (prevVer === "password" ? "text" : "password"));
-  }
-
   return (
-    <>
+    <div className="blog-container">
       <header>
         <img src="https://cdn4.iconfinder.com/data/icons/communication-v2/64/number_numero_count_thirty_five-2-512.png" alt="logo" />
-        <nav className={styles['nav-menu']}>
+        <nav className="nav-menu">
           <ul>
             <li>
               <Link to="/">Volver a la pagina principal</Link>
             </li>
+            <li>
+              <Link to="/blog">Publicar tu post</Link>
+            </li>
+            <li>
+              <Link to="https://www.mercadopago.com.ar/">Apoyanos</Link>
+            </li>
           </ul>
         </nav>
       </header>
-      <div className={styles['ContenedorGeneral']}>
-        {error && <h1 className={styles['admin-error']}>{error}</h1>}
-        <form className={styles['admin-form']}>
-          <input
-            className={styles['admin-input']}
-            value={pass}
-            type={ver}
-            placeholder="Ingresa la contraseña"
-            onChange={(e) => setPass(e.target.value)}
-          />
-          <button className={styles['admin-button']} onClick={ViewPass}>
-            {ver === "password" ? "Ver contraseña 🔐" : "Ocultar contraseña"}
-          </button>
-          <button className={styles['admin-button']} onClick={HandleSubmit}>
-            Enviar
-          </button>
-        </form>
-      </div>
-    </>
+      <form className="formulario" onSubmit={handleSubmit}>
+        <h4>Crea tu cuenta</h4>
+        <input
+          className="datos"
+          value={nombreUsuario}
+          type="text"
+          placeholder='Escribe tu nombre de usuario'
+          onChange={e => setNombreUsuario(e.target.value)}
+        />
+        <input
+          className="datos"
+          value={correo}
+          type="email"
+          placeholder='Escribe tu correo electrónico'
+          onChange={e => setCorreo(e.target.value)}
+        />
+        <input
+          className="datos"
+          value={contrasena}
+          type="password"
+          placeholder='Escribe tu contraseña'
+          onChange={e => setContrasena(e.target.value)}
+        />
+        <button className="boton">Crear Cuenta</button>
+      </form>
+    </div>
   );
 }
 
-export default Cuenta;
+export default CrearCuenta;
